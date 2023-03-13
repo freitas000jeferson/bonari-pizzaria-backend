a753ae66a36d1f23d28cfe7aba393ce4fa3aad4a
jeferson.freitas.glo <jeferson.freitas@globalhitss.com.br>
2023-03-13 11:27:40 -0300

    add constants

---
+++ src/common/constants/pieces-category.ts
@@ -0,0 +1,4 @@
+export const PiecesCategory = {
+  INTEIRA: 0,
+  METADE: 1,
+};
---
+++ src/common/constants/tax.ts
@@ -0,0 +1,12 @@
+export const TaxDelivery = {
+  ARACATI: 3,
+  PEDREGAL: 6,
+  RETIRADA: 0,
+};
+export const Tax = {
+  CREDITO: 4.99,
+  DEBITO: 0,
+  DINHEIRO: 0,
+  PIX: 0,
+};
+// value+(value*CREDITO/100)
